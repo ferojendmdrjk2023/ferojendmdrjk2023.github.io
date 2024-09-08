d4391f1e76cef5b7f448ccd6def616c706e8b8b4
ferojendmdrjk2023 <oldhabitsdiescreaming1989@gmail.com>
2024-09-08 09:10:26 +0700

    Add files via upload

---
+++ beecheck/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BEECHECK - Mainpage</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/global.css">
+    <link rel="stylesheet" href="styles/main.css">  
+</head>
+<body>
+    <div class="logo-ctn">
+        <script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.0/build/spline-viewer.js"></script>
+        <spline-viewer url="https://prod.spline.design/If8NJNFCtixuRjVJ/scene.splinecode"></spline-viewer>
+        
+    </div>
+    <div class="play-me">
+        <p style="position: absolute; top: 70%; left: 23%; transform: translate(-70%, -23%); text-align: center; ">Play with me with your mouse</p>
+    </div>
+        <section>
+        <div class="welcome-container"> 
+                <div class="logo-type">
+                    <h1>BEECHECK</h1>
+                    <div class= "Human-Art or AI-Art"><h2>Human-art or AI-art</h2></div>
+                    <h3>Test your art-sense</h3>
+                    <p style="padding-top: 50px;"><b>Be not afraid of AI-Generated Artwork, for the human is a better artist</b></p>
+                    <div class="btn-container">
+                        <a href="pages/login.php" class="btn">START</a>
+                    </div>
+                </div>
+        </section>        
+         </div>
+        
+</body>
+</html>
