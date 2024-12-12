7b7173b68b0c42935d9911cf50d8753716981c08
ferojendmdrjk2023 <oldhabitsdiescreaming1989@gmail.com>
2024-12-13 05:47:32 +0700

    Update app.js

--- anthology/app.js
+++ anthology/app.js
@@ -496,16 +496,6 @@ class TrackManager {
     }
 }
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
-        .then((registration) => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch((error) => {
-            console.error('Service Worker registration failed:', error);
-        });
-}
-
 
 // Instantiate and initialize the app
 let app = new SpotifyApp();
